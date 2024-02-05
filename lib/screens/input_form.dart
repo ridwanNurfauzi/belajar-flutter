077631419b1ee57a68d52466dc138f3c24be14f3
ridwanNurfauzi <mrnhp123@gmail.com>
2024-02-05 19:45:01 +0700

    edit

--- lib/screens/input_form.dart
+++ lib/screens/input_form.dart
@@ -35,134 +35,142 @@ class _BelajarFormState extends State<BelajarForm> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        height: double.infinity,
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Card(
-              elevation: 4,
-              margin: EdgeInsets.all(16),
-              shape: RoundedRectangleBorder(
-                side: BorderSide(color: Colors.black),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Padding(
-                padding: EdgeInsets.all(24),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Form biodata",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(height: 18),
-                      TextFormField(
-                        controller: namaController,
-                        decoration: InputDecoration(
-                          hintText: "Masukkan Nama",
-                          border: OutlineInputBorder(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Form"),
+      ),
+      body: SafeArea(
+        child: Container(
+          height: double.infinity,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Card(
+                elevation: 4,
+                margin: EdgeInsets.all(16),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Colors.black),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(24),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Form biodata",
+                          style: TextStyle(fontWeight: FontWeight.bold),
                         ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return "input nama";
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(
-                        height: 18,
-                      ),
-                      TextFormField(
-                        controller: jkController,
-                        decoration: InputDecoration(
-                          hintText: "Jenis Kelamin",
-                          border: OutlineInputBorder(),
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return "Input Jenis kelamin";
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(
-                        height: 18,
-                      ),
-                      TextFormField(
-                        controller: tglLahirController,
-                        decoration: InputDecoration(
-                          hintText: "Tanggal Lahir",
-                          border: OutlineInputBorder(),
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return "Input Tanggal Lahir";
-                          }
-                          return null;
-                        },
-                        onTap: () async {
-                          DateTime? pickedDate = await showDatePicker(
-                            context: context,
-                            initialDate: DateTime.now(),
-                            firstDate: DateTime(1900),
-                            lastDate: DateTime(2100),
-                          );
-                          if (pickedDate != null) {
-                            String tgl =
-                                DateFormat('yyyy-MM-dd').format(pickedDate);
-                            setState(() {
-                              tglLahirController.text = tgl;
-                            });
-                          } else {
-                            print("Tanggal tidak dipilih");
-                          }
-                        },
-                      ),
-                      SizedBox(
-                        height: 18,
-                      ),
-                      DropdownButtonFormField(
-                        decoration: InputDecoration(
-                            hintText: "Pilih Agama",
-                            labelText: "Agama",
-                            border: OutlineInputBorder()),
-                        items: agama.map((e) {
-                          int index = 0;
-                          return DropdownMenuItem(
-                            value: e,
-                            child: Text(e),
-                          );
-                        }).toList(),
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            _pilihAgama = newValue!;
-                          });
-                        },
-                      ),
-                      SizedBox(
-                        height: 18,
-                      ),
-                      SizedBox(
-                        width: displayWidth(context) * .8,
-                        height: displayHeight(context) * .075,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            _submit();
+                        SizedBox(height: 18),
+                        TextFormField(
+                          controller: namaController,
+                          decoration: InputDecoration(
+                            hintText: "Masukkan Nama",
+                            labelText: "Nama",
+                            border: OutlineInputBorder(),
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "input nama";
+                            }
+                            return null;
                           },
-                          child: Text("Simpan"),
                         ),
-                      ),
-                    ],
+                        SizedBox(
+                          height: 18,
+                        ),
+                        TextFormField(
+                          controller: jkController,
+                          decoration: InputDecoration(
+                            hintText: "Masukkan Jenis Kelamin",
+                            labelText: "Jenis Kelamin",
+                            border: OutlineInputBorder(),
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Input Jenis kelamin";
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(
+                          height: 18,
+                        ),
+                        TextFormField(
+                          controller: tglLahirController,
+                          decoration: InputDecoration(
+                            hintText: "Masukkan Tanggal Lahir",
+                            labelText: "Tanggal Lahir",
+                            border: OutlineInputBorder(),
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Input Tanggal Lahir";
+                            }
+                            return null;
+                          },
+                          onTap: () async {
+                            DateTime? pickedDate = await showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now(),
+                              firstDate: DateTime(1900),
+                              lastDate: DateTime(2100),
+                            );
+                            if (pickedDate != null) {
+                              String tgl =
+                                  DateFormat('yyyy-MM-dd').format(pickedDate);
+                              setState(() {
+                                tglLahirController.text = tgl;
+                              });
+                            } else {
+                              print("Tanggal tidak dipilih");
+                            }
+                          },
+                        ),
+                        SizedBox(
+                          height: 18,
+                        ),
+                        DropdownButtonFormField(
+                          decoration: InputDecoration(
+                              hintText: "Pilih Agama",
+                              labelText: "Agama",
+                              border: OutlineInputBorder()),
+                          items: agama.map((e) {
+                            int index = 0;
+                            return DropdownMenuItem(
+                              value: e,
+                              child: Text(e),
+                            );
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              _pilihAgama = newValue!;
+                            });
+                          },
+                        ),
+                        SizedBox(
+                          height: 18,
+                        ),
+                        SizedBox(
+                          width: displayWidth(context) * .8,
+                          height: displayHeight(context) * .075,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              _submit();
+                            },
+                            child: Text("Simpan"),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
